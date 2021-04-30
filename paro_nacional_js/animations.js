4f3a6060089f9c639fe470641127fa80184b941d
jhonatan <>
2021-04-30 12:26:44 -0500

    add css & js

---
+++ paro_nacional_js/animations.js
@@ -0,0 +1,49 @@
+var animated = document.querySelectorAll(".animated_1");
+
+function changeOpacity() {
+    for (var i=0; i < animated.length; i++ ) {
+        animated[i].style.opacity = 1;
+        animated[i].classList.add("showUp");
+    }
+}
+
+var animated_2 = document.querySelectorAll('.animated_2');
+
+function showScroll() {
+    var scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i < animated_2.length; i++ ) {
+        var animatedHigh = animated_2[i].offsetTop;
+        if (animatedHigh - 400 < scrollTop) {
+         animated_2[i].style.opacity = 1;
+         animated_2[i].classList.add("showUp");
+        }
+        else if (i == 3) {
+            if (animatedHigh - 700 < scrollTop) {
+             animated_2[i].style.opacity = 1;
+             animated_2[i].classList.add("showUp");
+            }
+
+        }
+    }
+}
+
+var animated_3 = document.querySelectorAll('.animated_3');
+
+function showScrollTwo() {
+    var scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i < animated_3.length; i++ ) {
+        console.log(i);
+        var animatedHigh = animated_3[i].offsetTop;
+        if (animatedHigh - 400 < scrollTop) {
+         animated_3[i].style.opacity = 1;
+         animated_3[i].classList.add("showLeft");
+        }
+    }
+}
+
+function showFunctions() {
+    showScroll();
+    showScrollTwo();
+}
+
+window.addEventListener("scroll", showFunctions);
